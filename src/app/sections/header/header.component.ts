438f007886a32350add8957236b4795f3b969662
LinaMarcelaPena <linapena112@gmail.com>
2022-12-22 13:03:36 -0500

    barra de navegacion

--- src/app/sections/header/header.component.ts
+++ src/app/sections/header/header.component.ts
@@ -5,6 +5,9 @@ import { Component } from '@angular/core';
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
+export class HeaderComponent  {
+
+  constructor() { }
+
 
 }
