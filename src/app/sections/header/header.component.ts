51eefc8f822fef8048cba5f8e1c2096ed06fbbdf
LinaMarcelaPena <linapena112@gmail.com>
2022-12-22 23:56:36 -0500

    menu hamburguesa responsive

--- src/app/sections/header/header.component.ts
+++ src/app/sections/header/header.component.ts
@@ -1,13 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent  {
+export class HeaderComponent implements OnInit {
 
   constructor() { }
 
+  ngOnInit(): void {
 
+    $('.menu-burger, .menu-items').on('click', function () {
+      $('.menu-bg, .menu-items, .menu-burger').toggleClass('fs');
+      $('.menu-burger').text() == "☰" ? $('.menu-burger').text('✕') : $('.menu-burger').text('☰');
+    });
+  }
 }
