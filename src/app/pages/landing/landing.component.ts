48aa1792b766acf48ce3d78ed2cf37152ac4c904
LinaMarcelaPena <linapena112@gmail.com>
2023-01-04 02:20:19 -0500

    carousel de anuncios funcional con  responsive

--- src/app/pages/landing/landing.component.ts
+++ src/app/pages/landing/landing.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CAROUSEL_DATA_ITEMS } from 'src/app/data/constants/carousel.const';
+import { ICarouselItem } from '../../sections/announcements/Icarousel-item.metadata';
 
 @Component({
   selector: 'app-landing',
   templateUrl: './landing.component.html',
   styleUrls: ['./landing.component.scss']
 })
-export class LandingComponent {
+export class LandingComponent implements OnInit {
+ 
+  public carouselData: ICarouselItem[]= CAROUSEL_DATA_ITEMS;
 
+  constructor () {
+
+  }
+
+  ngOnInit() {
+    
+  }
 }
---
+++ src/app/sections/announcements/Icarousel-item.metadata.ts
@@ -0,0 +1,14 @@
+//exportar la interfaz 
+//cuando se utilice el componente donde se va a emplear el carousel debe tener estas propiedades
+export interface ICarouselItem {
+    //id es necesario
+    id:number;
+    //si las imagenes del carousel llevan titulo y subtitulo se agregaran las propiedades correspondientes
+    link?: string;
+    //imagenes del carousel
+    image: string;
+    //orden con el ? se indica que es opcional
+    order?: number;
+    //espacio entre las imagenes del carousel tambien sera opcional
+    marginLeft?: number;
+} 
