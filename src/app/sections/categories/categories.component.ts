29b02d5787d443622bda3155ec097d38d83872c8
LinaMarcelaPena <linapena112@gmail.com>
2023-01-09 22:39:26 -0500

    responsive de la seccion categorias

--- src/app/sections/categories/categories.component.ts
+++ src/app/sections/categories/categories.component.ts
@@ -1,6 +1,7 @@
 import { Component, ElementRef, Input, OnInit } from '@angular/core';
 import { ISliderItem } from './Islider-item.metadata';
 
+
 @Component({
   selector: 'categories',
   templateUrl: './categories.component.html',
@@ -13,7 +14,7 @@ export class CategoriesComponent implements OnInit {
    * que utiliza el componente 
    */
   // el input se utiliza para definir propiedades que vienen desde una vista o un padre - altura del carousel
-  @Input() height = 249;
+  @Input() height = 170;
   // por si pasa el 100% de la pantalla
   @Input() isFullScreen = false;
   //los items van a utilizar las propiedades que creamos en la interfaz que por el momento va a ser un array vacio
@@ -28,22 +29,22 @@ export class CategoriesComponent implements OnInit {
   //inicializacion
   public finalHeight: string | number = 0;
   public currentPosition = 0;
-
+ 
 
   constructor() {
     //se asigna el valor que va a tener 
     //indicamos que si la altura final va a utiliza el 100% de la pantalla que tenga medida 
     // de 100 de alto y ancho si no es asi que utlice la altura que se envia por medio de la vista padre
     this.finalHeight = this.isFullScreen ? '100vh' : `${this.height}px`;
-
+    
   }
 
   ngOnInit() {
     //mapear los items para asignarle los valores de forma correcta 
     this.items.map((i, index) => {
       i.id = index;
-      //todas las imagenes estaran una seguida de otra 
-      i.marginLeft = 10;
+      //espacio entre imagenes
+        i.marginLeft = 5;
     });
   }
 
@@ -54,7 +55,7 @@ export class CategoriesComponent implements OnInit {
     // aqui se realiza la operacion para que nos cambie la posicion de cada imagen 
     //inicia con la posicion 0 al realizar la operacion  1 * -100px 
     //quedanra en la posicion 1
-    this.items.find((i) => i.id === 0)!.marginLeft = -15* position;
+    this.items.find((i) => i.id === 0)!.marginLeft = -6* position;
   }
 
   //muestra la posicion que contunia 
@@ -67,7 +68,7 @@ export class CategoriesComponent implements OnInit {
     //se coloca -1 ya que la posicion inicial sera 0
     if (nextPosition <= this.items.length - 1) {
       //si se cumple la condicion indicad anteriormente la posicion actual cambiara 
-      finalPercentage = -15 * nextPosition;
+      finalPercentage = -50 * nextPosition;
     } else {
       //al llegar al maximo de posiciones nos regresara a la posicion 0
       nextPosition = 0;
@@ -82,11 +83,11 @@ export class CategoriesComponent implements OnInit {
     let finalPercentage = 0;
     let backPosition = this.currentPosition - 1;
     if (backPosition >= 0) {
-      finalPercentage = -15 * backPosition;
+      finalPercentage = -50 * backPosition;
     } else {
       //nos enviara a la ultima posicion
       backPosition = this.items.length - 1;
-      finalPercentage = -15 * backPosition;
+      finalPercentage = -50 * backPosition;
     }
     this.items.find((i) => i.id === 0)!.marginLeft = finalPercentage;
     this.currentPosition = backPosition;
